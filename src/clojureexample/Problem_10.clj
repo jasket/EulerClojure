9cfade4e8a8df2baa9a5f2f13fba12ef95ed36fb
avanin <avanin@ecocompany.ru>
2016-11-15 17:37:48 +0300

    init

--- src/clojureexample/Problem_10.clj
+++ src/clojureexample/Problem_10.clj
@@ -2,38 +2,12 @@
 ; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 ; Find the sum of all the primes below two million.
 
-(defn modif
-  [a b]
-  (if (zero? b)
-     0
-     (if (zero? (mod a b)) 0 a)))
+(defn trans
+  [n arr]
+  (filter #(not= % 0) (map #(if (= (mod % n) 0) 0 %) arr)))
 
-(defn crop
-  [c acc]
-  (drop c acc))
-
-(defn rerange
-  [n range]
-  (let [r (map #(modif % n) range)]
-    (filter #(not= % 0) (crop 1 r))))
-
-(defn first'
-  [range]
-  (let [f (first range)]
-    (if (nil? f) 0 f)))
-
-(defn prime'
-  [n range res]
-  ((println n range res)
-    (if (empty? range)
-      res
-      (prime' (first' range) (rerange n range) (+ res (first' range))))))
-
-(defn prime [n]
-  (prime' 2 (range 2 n) 0))
-
-(prime 10)
-
-(rerange 5 (7))
-(map #(modif % 5) (7))
-(crop 1 (map #(< % 99) (range 7 8)))
+(defn eratosfen
+  [lim]
+  (loop [n 2, arr (range 2 lim), acc 0]
+    (let [lim' (trans n arr)]
+      (recur (first lim') lim' (+ n acc)))))
