ab4fae87dee38f5349ff26b6ba2da4e2bd02a7a1
jasket <avaninmail@gmail.com>
2016-11-28 23:05:12 +0300

    hm

--- src/clojureexample/problem_10.clj
+++ src/clojureexample/problem_10.clj
@@ -2,7 +2,8 @@
 ; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 ; Find the sum of all the primes below two million.
 
-(defn prime? [n]
-  (.isProbablePrime (BigInteger/valueOf n) 5))
-
-(+ 2 (reduce #(+ %1 %2) (filter prime? (take-nth 2 (range 1 2000000)))))
+(->> (range 1 2000000)
+     (take-nth 2)
+     (filter #(.isProbablePrime (BigInteger/valueOf %) 10))
+     (reduce +)
+     (+ 2))
